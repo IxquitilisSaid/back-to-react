41a8e0e1f5767850e5494e4728a34e5873bc8e2c
Jaime Oliveira <Jaime.Oliveira@softgarden.de>
2020-03-25 14:13:46 +0100

    how do you react people live with this meshuggah???

---
+++ src/components/ye-old-tome/YeOldTome.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './YeOldTome.css';
+
+
+function YeOldTome() {
+    return (
+        <div className="YeOldTome">
+            <div className="YeOldTome-back"></div>
+            <div className="YeOldTome-page6"></div>
+            <div className="YeOldTome-page5"></div>
+            <div className="YeOldTome-page4"></div>
+            <div className="YeOldTome-page3"></div>
+            <div className="YeOldTome-page2"></div>
+            <div className="YeOldTome-page1"></div>
+            <div className="YeOldTome-front"></div>
+            {/* <div className="YeOldTome-skrr"></div> */}
+        </div>
+    );
+}
+
+export default YeOldTome;
